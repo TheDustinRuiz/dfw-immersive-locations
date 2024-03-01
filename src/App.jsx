6daa0a5b3fbc0809c816276c7b727f36db4535bc
Dustin Ruiz <TheDustinRuiz@gmail.com>
2024-03-01 03:52:52 -0600

    Added title to React component and HTML title tag.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+const App = () => {
+
+  return (
+    <>
+      <h1>DFW Immersive Adventures: Explore Art, Culture, and Creativity</h1>
+    </>
+  )
+}
+
+export default App
